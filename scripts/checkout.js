bf7866719fdf1a0097d08cfcac8069e508536268
MennaAbd-elhady <menna258964@gmail.com>
2024-08-15 15:15:45 +0300

    activiating the delete button

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -13,7 +13,7 @@ cart.forEach((cartItem)=>{
   }));
  
   cartSummaryHTML+= `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingItem.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -97,6 +97,7 @@ document.querySelectorAll('.js-delete').forEach((link)=>{
   link.addEventListener(('click'),()=>{
     const productId=link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+    const container=document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
   });
 })
